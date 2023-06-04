fe080e84ea22eda8990d553ae0d8ae37bf7a56c6
sonysmile999@gmail.com <sonysmile999@gmail.com>
2023-06-04 08:26:21 +0300

    feat: Added a button to expand product cards.

--- src/components/UI/ProductsContent.vue
+++ src/components/UI/ProductsContent.vue
@@ -11,23 +11,37 @@
       }}
     </p>
   </div>
-  <div class="products-content__inner pt-12">
-    <ul>
-      <li  class="products-content__item bg-default w-331 rounded-20 shadow-border-ef-cards" v-for="item in productCards
-        " :key="item.id">
+  <div class="products-content__inner pt-12 ">
+    <ul class="flex justify-center grid grid-cols-3 gap-7 ">
+      <li
+          class="products-content__item bg-default w-331 rounded-20 shadow-border-ef-cards mx-auto cursor-pointer " v-for="item in
+      visibleProductCards" :key="item.id">
         <div class="products-content__item--box pt-10 pr-10 pb-10 pl-10">
-          <img  :src="`/src/assets/image/${item.img}`" :alt="item.alt"/>
-          <h2 class="text-center font-font-b text-18 leading-22 mb-5 mt-5">{{item.title}}</h2>
+          <img :src="`/src/assets/image/${item.img}`" :alt="item.alt"/>
+          <h2 class="text-center font-font-b text-18 leading-22 mb-5 mt-5">{{
+              item.title
+            }}</h2>
           <AddButton :text="btnText" :url="btnUrl"/>
         </div>
       </li>
     </ul>
+
   </div>
+  <button
+      class="products-content__item--load mt-10 justify-center flex mx-auto bg-c100 text-default text-16 leading-20 font-font-sb uppercase rounded-10 pt-5 pb-5 w-220 hover:bg-c101 "
+      @click="loadMoreCards"
+      v-if="hasHiddenCards">
+    {{
+      btnLoadText
+    }}
+  </button>
+
 </template>
 
 <script>
 import {useProductsStore} from "@/stores/products";
 import AddButton from "@/components/UI/AddButton.vue";
+import {ref, computed} from "vue";
 
 export default {
   name: "ProductsContent",
@@ -38,28 +52,70 @@ export default {
     btnUrl: {
       type: String,
       required: true,
-      default: '/'
-
+      default: '/',
     },
     btnText: {
       type: String,
       required: true,
       default: 'Add to cart',
     },
-
+    btnLoadText: {
+      type: String,
+      required: true,
+      default: '  See all products',
+    },
   },
   setup() {
     const productsStore = useProductsStore();
     const productTitle = productsStore.GET_TITLE;
     const productCards = productsStore.GET_PRODUCT_CARDS;
+
+
+    const visibleCardsCount = ref(3);
+    const increment = 3;
+
+    const visibleProductCards = computed(() => {
+      return productCards.slice(0, visibleCardsCount.value);
+    });
+    const hasHiddenCards = computed(() => {
+      return visibleCardsCount.value < productCards.length;
+    });
+
+    const loadMoreCards = () => {
+      const remainingCards = productCards.slice(visibleCardsCount.value);
+      const additionalCards = remainingCards.slice(0, increment);
+      visibleCardsCount.value += increment;
+      productCards.splice(visibleCardsCount.value, 0, ...additionalCards);
+    };
     return {
       productTitle,
-      productCards
+      productCards,
+      visibleProductCards,
+      hasHiddenCards,
+      loadMoreCards,
     };
-  }
-}
+  },
+
+
+};
 </script>
 
 <style lang="scss" scoped>
 @use "src/styles/variables" as var;
+.products-content{
+  &__item{
+    @extend %dtrans;
+    :hover{
+
+      @extend %htrans;
+      @extend %border-ef;
+    }
+    &--load{
+      @extend %dtrans;
+      :hover{
+        @extend %htrans;
+      }
+    }
+  }
+}
 </style>
--- src/stores/products.js
+++ src/stores/products.js
@@ -2,6 +2,7 @@ import {defineStore} from 'pinia';
 
 export const useProductsStore = defineStore('products', {
     state: () => ({
+
         productTitle: {
             title: 'Our Shop',
             desc: 'Join our kitesurf lovers worldwide who trust us to provide the best kite gear and deals.',
@@ -13,15 +14,49 @@ export const useProductsStore = defineStore('products', {
                 img: 'product-img-1.webp',
                 alt: 'product-img'
             },
+            {
+                id: 2,
+                title: 'Summer surfboat 6ft',
+                img: 'product-img-2.webp',
+                alt: 'product-img'
+            },
+            {
+                id: 3,
+                title: 'Summer surfboat 6ft',
+                img: 'product-img-3.webp',
+                alt: 'product-img'
+            },
+            {
+                id: 4,
+                title: 'Summer surfboat 6ft',
+                img: 'product-img-1.webp',
+                alt: 'product-img'
+            },
+            {
+                id: 5,
+                title: 'Summer surfboat 6ft',
+                img: 'product-img-2.webp',
+                alt: 'product-img'
+            },
+            {
+                id: 6,
+                title: 'Summer surfboat 6ft',
+                img: 'product-img-3.webp',
+                alt: 'product-img'
+            },
+
         ],
+
     }),
     getters: {
         GET_TITLE: (state) => state.productTitle,
         GET_PRODUCT_CARDS: (state) => state.productCards,
+
     },
     actions: {
         SET_TITLE(productTitle) {
             this.productTitle = productTitle;
         },
+
     },
 });
