4d897a2cf36cd9b285788d46a7f5ba88d75ba1e9
sonysmile999@gmail.com <sonysmile999@gmail.com>
2023-06-22 08:08:36 +0300

    feat: I finished the banner component and created a custom button.

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="container-1320">
-  <BannerContent  :style="bg"/>
+  <BannerContent class="rounded-20"  :style="bg" :title="title" :desc="desc"/>
 </div>
 </template>
 
@@ -10,6 +10,20 @@ import bgImage from  '@/assets/image/bg-banner.webp';
 export default {
   name: "Banner",
   components: {BannerContent},
+  props: {
+    title: {
+      type: String,
+      required: true,
+      default: 'Do You Want to Start Surfing Now?',
+    },
+    desc:{
+      type: String,
+      required: true,
+      default:
+        'Regardless of what stage of life you’re in, we believe that experiencing a new place is always better when shared.',
+    },
+
+  },
   computed: {
     bg() {
       return {
--- src/components/UI/BannerContent.vue
+++ src/components/UI/BannerContent.vue
@@ -1,18 +1,49 @@
 <template>
-<div class="banner-conyent">
-  <img src="" alt="">
-  <h2></h2>
-  <p></p>
-
-</div>
+  <div class="banner-content text-center  pt-16 mt-32 mb-5">
+    <img class="mx-auto pb-5" :src="elImg"/>
+    <h2 class="text-default text-38 leading-42 font-font-bl mb-3">{{
+        title
+      }}</h2>
+    <p class="text-16 leading-20 font-font-l text-default pb-8 w-623 mx-auto"> {{
+        desc
+      }}</p>
+    <Button class="mb-28" :url="btnUrl" :buttonText="btnText" />
+  </div>
 </template>
 
 <script>
-
+import {ref} from 'vue'
+import Button from "@/components/UI/Button.vue";
 
 export default {
   name: "BannerContent",
-  components: {},
+  components: {Button},
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
+
+  },
+  setup() {
+    const elImg = ref('');
+    const fetchImage = () => {
+      const elImgPath = 'src/assets/image/svg/element-banner.svg';
+      elImg.value = elImgPath;
+    };
+    const btnUrl = ref('https://google.com');
+    const  btnText = ref('Start Now');
+    fetchImage();
+    return {
+      btnUrl,
+      btnText,
+      elImg,
+    }
+  },
 }
 </script>
 
